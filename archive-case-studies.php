a351e0e5fe2c1243fc31470cb38703e7f3b7a0bc
ChillibyteUK <dan@chillibyte.co.uk>
2023-04-20 11:54:38 +0100

    cs a

--- archive-case-studies.php
+++ archive-case-studies.php
@@ -2,26 +2,18 @@
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 
-$page_for_posts = get_option( 'page_for_posts' );
-$bg = get_the_post_thumbnail_url($page_for_posts,'full');
-
 get_header();
 ?>
 <main id="main">
 <!-- hero -->
-<section id="hero" class="hero d-flex align-items-center hero--default">
-    <div class="overlay"></div>
-    <div class="hero__inner container-xl">
-        <div class="row h-100">
-            <div class="col-lg-6 hero__content d-flex flex-column justify-content-center order-2 order-lg-1 py-5" data-aos="fade">
-                <h1>Case Studies</h1>
-            </div>
-            <div class="col-lg-6 hero__image order-1 order-lg-2" style="background-image:url(<?=$bg?>)">
-            </div>
-        </div>
+<main id="main">
+<header class="hero hero--short">
+    <div class="hero_bg" style="background-image: url(/wp-content/uploads/2023/01/hero-home.jpg)"></div>
+    <div class="container-xl">
+        <h1>Read our <span>Case Studies</span></h1>
+        <div class="mw-md-75 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam architecto ad, dolore amet temporibus obcaecati voluptatibus ducimus velit possimus dignissimos!</div>
     </div>
-    <div class="overlay--bottom"></div>
-</section>
+</header>
 </main>
 <?php
 get_footer();
