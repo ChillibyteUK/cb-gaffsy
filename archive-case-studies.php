91ed053db0c662ebe8bf23a2016f4914bdb31a9d
ChillibyteUK <dan@chillibyte.co.uk>
2023-04-20 11:29:53 +0100

    cs archive

--- archive-case-studies.php
+++ archive-case-studies.php
@@ -2,12 +2,26 @@
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 
+$page_for_posts = get_option( 'page_for_posts' );
+$bg = get_the_post_thumbnail_url($page_for_posts,'full');
+
 get_header();
 ?>
 <main id="main">
-    <div class="container py-5">
-        <h1 class="mb-4">Case Studies</h1>
+<!-- hero -->
+<section id="hero" class="hero d-flex align-items-center hero--default">
+    <div class="overlay"></div>
+    <div class="hero__inner container-xl">
+        <div class="row h-100">
+            <div class="col-lg-6 hero__content d-flex flex-column justify-content-center order-2 order-lg-1 py-5" data-aos="fade">
+                <h1>Case Studies</h1>
+            </div>
+            <div class="col-lg-6 hero__image order-1 order-lg-2" style="background-image:url(<?=$bg?>)">
+            </div>
+        </div>
     </div>
+    <div class="overlay--bottom"></div>
+</section>
 </main>
 <?php
 get_footer();
