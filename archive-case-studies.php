7110149608900e74c979cd9ae483644f56d87849
ChillibyteUK <dan@chillibyte.co.uk>
2023-04-20 11:16:25 +0100

    start of case studies

---
+++ archive-case-studies.php
@@ -0,0 +1,13 @@
+<?php
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+<main id="main">
+    <div class="container py-5">
+        <h1 class="mb-4">Case Studies</h1>
+    </div>
+</main>
+<?php
+get_footer();
