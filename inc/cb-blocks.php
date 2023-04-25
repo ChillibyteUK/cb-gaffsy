8db397f926efad90be391ceeb12ba271c2ff6721
ChillibyteUK <dan@chillibyte.co.uk>
2023-04-25 10:10:09 +0100

    case studies with excerpt

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -212,6 +212,16 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_case_studies_excerpt',
+            'title'				=> __('CB Case Studies w Excerpt'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_case_studies_excerpt.php',
+            'keywords'			=> array( 'case','studies', 'excerpt' ),
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
