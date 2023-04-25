d91b59adce6736a85582ca54546751158c66c92d
ChillibyteUK <dan@chillibyte.co.uk>
2023-04-25 08:59:42 +0100

    pull from branch

--- archive-case-studies.php
+++ archive-case-studies.php
@@ -6,66 +6,82 @@ get_header();
 ?>
 <!-- hero -->
 <main id="main" class="caseStudies">
-<header class="hero hero--short">
-    <div class="hero_bg" style="background-image: url(/wp-content/uploads/2023/01/hero-home.jpg)"></div>
-    <div class="container-xl">
-        <h1>Read our <span>Case Studies</span></h1>
-        <div class="mw-md-75 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam architecto ad, dolore amet temporibus obcaecati voluptatibus ducimus velit possimus dignissimos!</div>
-    </div>
-</header>
-<div class="container-xl pb-5">
-    <div class="mb-4" id="filters">
+    <header class="hero hero--short">
+        <div class="hero_bg" style="background-image: url(/wp-content/uploads/2023/01/hero-home.jpg)"></div>
+        <div class="container-xl">
+            <h1>Read our <span>Case Studies</span></h1>
+            <div class="mw-md-75 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+                architecto ad, dolore amet temporibus obcaecati voluptatibus ducimus velit possimus dignissimos!</div>
+        </div>
+    </header>
+    <div class="container-xl pb-5">
+        <div class="mb-4" id="filters">
             <?php
         $terms = get_terms(
-            array(
+    array(
                 'taxonomy'   => 'cstypes',
                 'hide_empty' => true,
                 'order' => 'DESC',
             )
-        );
-            ?>
-        <div class="option-set mb-2" data-group="cstype">
-            <input type="checkbox" class="form-check-input all" value="" id="type-all" checked>
-            <label for="type-all" class="form-check-label">All Types</label>
-            <?php
-            foreach ($terms as $term) {
-                ?>
-                <input type="checkbox" class="form-check-input" value=".<?=$term->slug?>" id="<?=$term->slug?>">
-                <label class="form-check-label" for="<?=$term->slug?>"><?=$term->name?></label>
+);
+?>
+            <div class="option-set mb-2" data-group="cstype">
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input all" value="" id="type-all" checked>
+                    <label for="type-all" class="form-check-label">All Types</label>
+                </div>
                 <?php
-            }
-            ?>
-        </div>
+foreach ($terms as $term) {
+    ?>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input"
+                        value=".<?=$term->slug?>"
+                        id="<?=$term->slug?>">
+                    <label class="form-check-label"
+                        for="<?=$term->slug?>"><?=$term->name?></label>
+                </div>
+                <?php
+}
+?>
+            </div>
+            <hr>
             <?php
         $terms = get_terms(
-            array(
+    array(
                 'taxonomy'   => 'locations',
                 'hide_empty' => true,
                 'order' => 'DESC',
             )
-        );
-            ?>
-        <div class="option-set mb-2" data-group="location">
-            <input type="checkbox" value="" id="loca-all" class="form-check-input all" checked>
-            <label for="loca-all" class="form-check-label">All Locations</label>
-            <?php
-            foreach ($terms as $term) {
-                ?>
-                <input type="checkbox" class="form-check-input" value=".<?=$term->slug?>" id="<?=$term->slug?>">
-                <label for="<?=$term->slug?>" class="form-check-label"><?=$term->name?></label>
+);
+?>
+            <div class="option-set mb-2" data-group="location">
+                <div class="form-check">
+                    <input type="checkbox" value="" id="loca-all" class="form-check-input all" checked>
+                    <label for="loca-all" class="form-check-label">All Locations</label>
+                </div>
                 <?php
-            }
-            ?>
+foreach ($terms as $term) {
+    ?>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input"
+                        value=".<?=$term->slug?>"
+                        id="<?=$term->slug?>">
+                    <label for="<?=$term->slug?>"
+                        class="form-check-label"><?=$term->name?></label>
+                </div>
+                <?php
+}
+?>
+            </div>
+            <div class="status">
+                <div class="count"><span class="filter-count"></span> items found.</div>
+            </div>
         </div>
-        <div class="status">
-            <div class="count"><span class="filter-count"></span> items found.</div>
-        </div>
-    </div>
 
-    <p id="filter-display"></p>
+        <p id="filter-display"></p>
 
-    <div class="w-100" id="grid">
-        <?php
+        <div class="w-100" id="grid">
+            <?php
     while (have_posts()) {
         the_post();
         $img = get_the_post_thumbnail_url(get_the_ID(), 'large');
@@ -81,68 +97,82 @@ get_header();
         $catclass .= ' ' . implode(' ', array_map('cbslugify', $type));
 
         $the_date = get_the_date('jS F, Y');
+
+        $slug = acf_slugify(basename(get_the_permalink()));
         ?>
-        <div class="<?=$catclass?> caseStudy w-100 mb-4">
-            <div class="caseStudy_card">
-                <img class="caseStudy_card__image" src="<?=$img?>">
-                <div class="caseStudy_card__content">
-                    <div class="article-title mb-2">
-                        <?=get_the_title()?>
+            <div class="<?=$catclass?> caseStudy w-100 mb-4">
+                <a class="anchor" id="<?=$slug?>"></a>
+                <div class="caseStudy_card">
+                    <div class="caseStudy_card__image">
+                        <img src="<?=$img?>" alt="">
                     </div>
-                    <div class="article-body">
-                        <?=get_the_content()?>
-                    </div>
-                    <?php
+                    <div class="caseStudy_card__content">
+                        <div class="article-title mb-2">
+                            <?=get_the_title()?>
+                        </div>
+                        <div class="article-body">
+                            <?=get_the_content()?>
+                        </div>
+                        <?php
                     if (get_field('quote')) {
                         ?>
-                    <div class="article-quote">
-                        <?=apply_filters('the_content',get_field('quote'))?>
-                    </div>
-                        <?php
+                        <div class="quote-container">
+                            <div class="article-quote">
+                                <?=apply_filters('the_content', get_field('quote'))?>
+                            </div>
+                            <?php
                         if (get_field('attribution')) {
                             ?>
-                    <div class="article-attrib"><?=get_field('attribution')?></div>
+                            <div class="article-attrib">
+                                <?=get_field('attribution')?>
+                            </div>
                             <?php
                         }
-                    }
-                    ?>
-                    <div class="article-highlights">
+                        ?>
+                        </div>
                         <?php
-                        $hl = get_field('highlights');
-                        if ($hl['offered'] ?? null) {
-                            ?>
+                    }
+        ?>
+                        <div class="article-highlights">
+                            <?php
+            $hl = get_field('highlights');
+        if ($hl['offered'] ?? null) {
+            ?>
                             <div>
-                                <img decoding="async" src="/wp-content/uploads/2023/01/icon__free-cash-offer.svg" width="60" height="40">
+                                <img decoding="async" src="/wp-content/uploads/2023/01/icon__free-cash-offer.svg"
+                                    width="60" height="40">
                                 <span><?=$hl['offered']?></span>
                             </div>
                             <?php
-                        }
-                        if ($hl['duration'] ?? null) {
-                            ?>
+        }
+        if ($hl['duration'] ?? null) {
+            ?>
                             <div>
-                                <img decoding="async" src="/wp-content/uploads/2023/02/icon__complete.svg" width="60" height="40">
+                                <img decoding="async" src="/wp-content/uploads/2023/02/icon__complete.svg" width="60"
+                                    height="40">
                                 <span><?=$hl['duration']?></span>
                             </div>
                             <?php
-                        }
-                        if ($hl['price'] ?? null) {
-                            ?>
+        }
+        if ($hl['price'] ?? null) {
+            ?>
                             <div>
-                                <img decoding="async" src="/wp-content/uploads/2023/01/icon__no-fees.svg" width="60" height="40">
+                                <img decoding="async" src="/wp-content/uploads/2023/01/icon__no-fees.svg" width="60"
+                                    height="40">
                                 <span><?=$hl['price']?></span>
                             </div>
                             <?php
-                        }
-                        ?>
+        }
+        ?>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <?php
+            <?php
     }
-    ?>
+?>
+        </div>
     </div>
-</div>
 </main>
 <?php
 add_action('wp_footer', function () {
@@ -165,32 +195,34 @@ add_action('wp_footer', function () {
 
         $container.isotope();
         // do stuff when checkbox change
-        $('#filters').on( 'change', function( jQEvent ) {
-            var $checkbox = $( jQEvent.target );
-            manageCheckbox( $checkbox );
+        $('#filters').on('change', function(jQEvent) {
+            var $checkbox = $(jQEvent.target);
+            manageCheckbox($checkbox);
 
-            var comboFilter = getComboFilter( filters );
+            var comboFilter = getComboFilter(filters);
 
-            $container.isotope({ filter: comboFilter });
+            $container.isotope({
+                filter: comboFilter
+            });
 
             // $filterDisplay.text( comboFilter );
             updateFilterCount();
         });
 
-        function getComboFilter( filters ) {
+        function getComboFilter(filters) {
 
             var i = 0;
             var comboFilters = [];
             var message = [];
 
-            for ( var prop in filters ) {
-                message.push( filters[ prop ].join(' ') );
-                var filterGroup = filters[ prop ];
+            for (var prop in filters) {
+                message.push(filters[prop].join(' '));
+                var filterGroup = filters[prop];
                 // skip to next filter group if it doesn't have any values
-                if ( !filterGroup.length ) {
+                if (!filterGroup.length) {
                     continue;
                 }
-                if ( i === 0 ) {
+                if (i === 0) {
                     // copy to new array
                     comboFilters = filterGroup.slice(0);
                 } else {
@@ -198,9 +230,9 @@ add_action('wp_footer', function () {
                     // copy to fresh array
                     var groupCombo = comboFilters.slice(0); // [ A, B ]
                     // merge filter Groups
-                    for (var k=0, len3 = filterGroup.length; k < len3; k++) {
-                        for (var j=0, len2 = groupCombo.length; j < len2; j++) {
-                            filterSelectors.push( groupCombo[j] + filterGroup[k] ); // [ 1, 2 ]
+                    for (var k = 0, len3 = filterGroup.length; k < len3; k++) {
+                        for (var j = 0, len2 = groupCombo.length; j < len2; j++) {
+                            filterSelectors.push(groupCombo[j] + filterGroup[k]); // [ 1, 2 ]
                         }
                     }
                     // apply filter selectors to combo filters for next group
@@ -213,41 +245,46 @@ add_action('wp_footer', function () {
             return comboFilter;
         }
 
-        function manageCheckbox( $checkbox ) {
+        function manageCheckbox($checkbox) {
             var checkbox = $checkbox[0];
 
             var group = $checkbox.parents('.option-set').attr('data-group');
             // create array for filter group, if not there yet
-            var filterGroup = filters[ group ];
-            if ( !filterGroup ) {
-                filterGroup = filters[ group ] = [];
+            var filterGroup = filters[group];
+            if (!filterGroup) {
+                filterGroup = filters[group] = [];
             }
 
             var isAll = $checkbox.hasClass('all');
             // reset filter group if the all box was checked
-            if ( isAll ) {
-                delete filters[ group ];
-                if ( !checkbox.checked ) {
+            if (isAll) {
+                delete filters[group];
+                if (!checkbox.checked) {
                     checkbox.checked = 'checked';
                 }
             }
             // index of
-            var index = $.inArray( checkbox.value, filterGroup );
+            // var index = $.inArray(checkbox.value, filterGroup);
+            var index = $checkbox.parent().siblings().find('input').index(checkbox.value);
 
-              if ( checkbox.checked ) {
+            if (checkbox.checked) {
                 var selector = isAll ? 'input' : 'input.all';
-                $checkbox.siblings( selector ).removeAttr('checked');
-                if ( !isAll && index === -1 ) {
+                // $checkbox.siblings(selector).removeAttr('checked');
+                $checkbox.parent().siblings().find(selector).removeAttr('checked');
+                if (!isAll && index === -1) {
                     // add filter to group
-                    filters[ group ].push( checkbox.value );
+                    filters[group].push(checkbox.value);
                 }
 
-            } else if ( !isAll ) {
+            } else if (!isAll) {
                 // remove filter from group
-                filters[ group ].splice( index, 1 );
+                filters[group].splice(index, 1);
                 // if unchecked the last box, check the all
-                if ( !$checkbox.siblings('[checked]').length ) {
-                    $checkbox.siblings('input.all').attr('checked', 'checked');
+                // if (!$checkbox.siblings('[checked]').length) {
+                //     $checkbox.siblings('input.all').attr('checked', 'checked');
+                if (!$checkbox.parent().siblings().find('[checked]').length) {
+                    $checkbox.parent().siblings().find('input.all').attr('checked', 'checked');
+
                 }
             }
         }
@@ -265,3 +302,4 @@ add_action('wp_footer', function () {
 }, 9999);
 
 get_footer();
+?>
