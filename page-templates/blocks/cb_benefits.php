b618691021c7b9037e9d858c3acc99161ce6a21f
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-04-26 15:04:33 +0100

    a11y benefits

--- page-templates/blocks/cb_benefits.php
+++ page-templates/blocks/cb_benefits.php
@@ -22,10 +22,20 @@
                 <h3 class="text-center mb-4">
                     <?=get_field('right_title')?>
                 </h3>
+                <?php
+                $img1 = wp_get_attachment_image_url(get_field('card_icon_1'),'full');
+                preg_match('/^.*__(.*)\.svg/', $img1, $alt1);
+                $img2 = wp_get_attachment_image_url(get_field('card_icon_2'),'full');
+                preg_match('/^.*__(.*)\.svg/', $img2, $alt2);
+                $img3 = wp_get_attachment_image_url(get_field('card_icon_3'),'full');
+                preg_match('/^.*__(.*)\.svg/', $img3, $alt3);
+                $img4 = wp_get_attachment_image_url(get_field('card_icon_4'),'full');
+                preg_match('/^.*__(.*)\.svg/', $img4, $alt4);
+                ?>
                 <div class="row g-4">
                     <div class="col-md-6">
                         <div class="benefits__card">
-                            <div class="benefits__icon"><img src="<?=wp_get_attachment_image_url(get_field('card_icon_1'),'full')?>"></div>
+                            <div class="benefits__icon"><img src="<?=$img1?>" alt="<?=$alt1[1]?> icon" width=120 height=96></div>
                             <h4><?=get_field('card_title_1')?>
                             </h4>
                             <p><?=get_field('card_content_1')?>
@@ -34,7 +44,7 @@
                     </div>
                     <div class="col-md-6">
                         <div class="benefits__card">
-                            <div class="benefits__icon"><img src="<?=wp_get_attachment_image_url(get_field('card_icon_2'),'full')?>"></div>
+                            <div class="benefits__icon"><img src="<?=$img2?>" alt="<?=$alt2[1]?> icon" width=120 height=96></div>
                             <h4><?=get_field('card_title_2')?>
                             </h4>
                             <p><?=get_field('card_content_2')?>
@@ -43,7 +53,7 @@
                     </div>
                     <div class="col-md-6">
                         <div class="benefits__card">
-                            <div class="benefits__icon"><img src="<?=wp_get_attachment_image_url(get_field('card_icon_3'),'full')?>"></div>
+                            <div class="benefits__icon"><img src="<?=$img3?>" alt="<?=$alt3[1]?> icon" width=120 height=96></div>
                             <h4><?=get_field('card_title_3')?>
                             </h4>
                             <p><?=get_field('card_content_3')?>
@@ -52,7 +62,7 @@
                     </div>
                     <div class="col-md-6">
                         <div class="benefits__card">
-                            <div class="benefits__icon"><img src="<?=wp_get_attachment_image_url(get_field('card_icon_4'),'full')?>"></div>
+                            <div class="benefits__icon"><img src="<?=$img4?>" alt="<?=$alt4[1]?> icon" width=120 height=96></div>
                             <h4><?=get_field('card_title_4')?>
                             </h4>
                             <p><?=get_field('card_content_4')?>
