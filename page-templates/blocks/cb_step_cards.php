be49c33c12b304371eae64bda345c87611f2c421
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-04-26 15:00:13 +0100

    a11y step cards

--- page-templates/blocks/cb_step_cards.php
+++ page-templates/blocks/cb_step_cards.php
@@ -6,6 +6,12 @@
         <h2 class="text-center pb-5"><?=get_field('title')?></h2>
             <?php
         }
+        $img1 = wp_get_attachment_image_url(get_field('icon_1'),'full');
+        preg_match('/^.*__(.*)\.svg/', $img1, $alt1);
+        $img2 = wp_get_attachment_image_url(get_field('icon_2'),'full');
+        preg_match('/^.*__(.*)\.svg/', $img2, $alt2);
+        $img3 = wp_get_attachment_image_url(get_field('icon_3'),'full');
+        preg_match('/^.*__(.*)\.svg/', $img3, $alt3);
         ?>
         <div class="row mx-0 mb-5 g-5 g-lg-4">
             <div class="col-lg-4">
@@ -13,7 +19,7 @@
                     <div class="card__step" data-aos="zoom-in" data-aos-delay="250">
                         <p>1</p>
                     </div>
-                    <div class="card__icon"><img src="<?=wp_get_attachment_image_url(get_field('icon_1'),'full')?>" width=120 height=80></div>
+                    <div class="card__icon"><img src="<?=$img1?>" width=120 height=80 alt="<?=$alt1[1]?> icon"></div>
                     <div class="card__title">
                         <h3><?=get_field('title_1')?>
                         </h3>
@@ -28,7 +34,7 @@
                     <div class="card__step" data-aos="zoom-in" data-aos-delay="400">
                         <p>2</p>
                     </div>
-                    <div class="card__icon"><img src="<?=wp_get_attachment_image_url(get_field('icon_2'),'full')?>" width=120 height=80></div>
+                    <div class="card__icon"><img src="<?=$img2?>" width=120 height=80 alt="<?=$alt2[1]?> icon"></div>
                     <div class="card__title">
                         <h3><?=get_field('title_2')?>
                         </h3>
@@ -43,7 +49,7 @@
                     <div class="card__step" data-aos="zoom-in" data-aos-delay="550">
                         <p>3</p>
                     </div>
-                    <div class="card__icon"><img src="<?=wp_get_attachment_image_url(get_field('icon_3'),'full')?>" width=120 height=80></div>
+                    <div class="card__icon"><img src="<?=$img3?>" width=120 height=80 alt="<?=$alt3[1]?> icon"></div>
                     <div class="card__title">
                         <h3><?=get_field('title_3')?>
                         </h3>
