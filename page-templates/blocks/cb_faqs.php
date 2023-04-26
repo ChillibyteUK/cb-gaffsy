02e4391e366172e004f86e4632aeed3a48f21751
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-04-26 14:17:04 +0100

    a11y for accordion

--- page-templates/blocks/cb_faqs.php
+++ page-templates/blocks/cb_faqs.php
@@ -29,7 +29,7 @@ $theme = get_field('theme');
         $ac = $accordion . '_' . $counter;
         ?>
         <div itemscope="" itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
-            <div class="accordion-head accordion-collapse <?=$collapsed?>" itemprop="name" data-bs-toggle="collapse" id="heading_<?=$ac?>" data-bs-target="#c<?=$ac?>" aria-role="button" aria-expanded="true" aria-controls="collapse_<?=$ac?>">
+            <div class="accordion-head accordion-collapse <?=$collapsed?>" itemprop="name" data-bs-toggle="collapse" id="heading_<?=$ac?>" data-bs-target="#c<?=$ac?>" role="button" aria-expanded="true" aria-controls="c<?=$ac?>">
                 <div class="pb-1"><?=get_sub_field('question')?></div>
             </div>
             <div class="collapse <?=$show?>" itemscope="" itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" id="c<?=$ac?>" aria-labelledby="heading_<?=$ac?>" data-bs-parent="#accordion<?=$accordion?>">
