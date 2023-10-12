5ddda6f77f6eb2ad0ca339539ab9b7325bf100ae
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-10-12 11:53:04 +0100

    team cols to repeater

--- page-templates/blocks/cb_team_cols.php
+++ page-templates/blocks/cb_team_cols.php
@@ -1,23 +1,22 @@
 <section class="team_cols">
     <div class="container-xl py-5">
         <h2><?=get_field('title')?></h2>
-        <div class="row g-4">
+        <div class="row g-4 justify-content-center">
+            <?php
+            while(have_rows('members')) {
+                the_row();
+                ?>
             <div class="col-lg-6">
                 <div class="team_cols__img">
-                    <img src="<?=wp_get_attachment_image_url(get_field('left_image'),'full')?>">
+                    <img src="<?=wp_get_attachment_image_url(get_sub_field('image'),'full')?>">
                 </div>
-                <div class="team_cols__name"><?=get_field('left_name')?></div>
-                <div class="team_cols__role"><?=get_field('left_role')?></div>
-                <div class="team_cols__bio"><?=get_field('left_bio')?></div>
+                <div class="team_cols__name"><?=get_sub_field('name')?></div>
+                <div class="team_cols__role"><?=get_sub_field('role')?></div>
+                <div class="team_cols__bio"><?=get_sub_field('bio')?></div>
             </div>
-            <div class="col-lg-6">
-                <div class="team_cols__img">
-                    <img src="<?=wp_get_attachment_image_url(get_field('right_image'),'full')?>">
-                </div>
-                <div class="team_cols__name"><?=get_field('right_name')?></div>
-                <div class="team_cols__role"><?=get_field('right_role')?></div>
-                <div class="team_cols__bio"><?=get_field('right_bio')?></div>
-            </div>
+                <?php
+            }
+            ?>
         </div>
     </div>
 </section>
