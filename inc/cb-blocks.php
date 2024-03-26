31f26bfd7b28f22299a41bb3c6f14cfa7601cd4e
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-03-26 14:58:20 +0000

    lease calc

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -245,6 +245,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_lease_calc',
+            'title'				=> __('CB Lease Calculator'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_lease_calc.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
---
+++ page-templates/blocks/cb_lease_calc.php
@@ -0,0 +1,70 @@
+<style>
+    .lease_calc label {
+        font-weight: bold;
+    }
+</style>
+<section class="lease_calc py-5 mt-5">
+    <div class="container-xl">
+        <h2>Lease Extension Calculator</h2>
+        <div class="row">
+            <div class="col-lg-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aspernatur ullam officiis reprehenderit voluptates vitae esse dicta distinctio sunt recusandae?
+            </div>
+            <div class="col-lg-6">
+                <div class="row g-4 mb-4">
+                    <div class="col-lg-5">
+                        <label for="term">Years left on lease</label>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="term_inner">
+                            <div class="text-center" id="rangeval">70</div>
+                            <input type="range" id="term" class="form-range" min=5 max=125 value="70" name="term" onInput="document.getElementById('rangeval').innerText = document.getElementById('term').value">
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <label for="rent">Ground Rent <small>(average)</small></label>
+                    </div>
+                    <div class="col-lg-7 d-flex align-items-center">
+                        &pound;&nbsp;<input type="number" class="form-control w-75 me-1" name="rent" id="rent" min="0" value="75" style="width: auto;">
+                        <small style="display:inline-block;white-space:nowrap;">per annum</small>
+                    </div>
+                    <div class="col-lg-5">
+                        <label for="rent">Property Value <small>(if extended)</small></label>
+                    </div>
+                    <div class="col-lg-7 d-flex align-items-center">
+                        &pound;&nbsp;<input type="number" class="form-control" name="value" id="value" min=0 value=595000>
+                    </div>
+                </div>
+                <div class="text-center mb-4">
+                    <button class="btn btn-primary" id="calc">Calculate the Cost</button>
+                </div>
+                <div class="py-2 text-center fs-4" id="total"></div>
+            </div>
+        </div>
+    </div>
+</section>
+<script>
+
+function calculateTotal() {
+    var rent = parseFloat(document.getElementById('rent').value);
+    var term = parseInt(document.getElementById('term').value);
+    var value = parseFloat(document.getElementById('value').value);
+
+    var additionalValue;
+    if (term <= 80) {
+        additionalValue = value * 0.1;
+    } else if (term <= 90) {
+        additionalValue = value * 0.2;
+    } else {
+        additionalValue = value * 0.3;
+    }
+
+    var total = (rent * term) + additionalValue;
+
+    document.getElementById('total').innerText = 'Expected Premium: £' + Math.round(total).toLocaleString();
+}
+
+document.addEventListener('DOMContentLoaded', calculateTotal);
+document.getElementById('calc').addEventListener('click', calculateTotal);
+
+</script>
